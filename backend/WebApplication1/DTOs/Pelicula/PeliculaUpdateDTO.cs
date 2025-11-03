8d9de61bc50e423f92444e4cce14e3c2a2a61918
421301-1w1-llop-julian <421301@tecnicatura.frc.utn.edu.ar>
2025-11-03 19:45:55 -0300

    Mas DOTs

---
+++ backend/WebApplication1/DTOs/Pelicula/PeliculaUpdateDTO.cs
@@ -0,0 +1,21 @@
+﻿namespace WebApplication1.DTOs.Pelicula
+{
+    public class PeliculaUpdateDTO
+    {
+        public int IdPelicula { get; set; }
+        public string? Nombre { get; set; }
+        public string? Descripcion { get; set; }
+        public int? Duracion { get; set; }
+        public DateOnly? FechaEstreno { get; set; }
+        public int? IdClasificacion { get; set; }
+        public int? IdPais { get; set; }
+        public int? IdDistribuidora { get; set; }
+        public int? IdTipoPublico { get; set; }
+
+        // Actualizaciones de relaciones (opcionales)
+        public List<int>? GeneroIds { get; set; }
+        public List<int>? IdiomaIds { get; set; }
+        public List<int>? ActorIds { get; set; }
+        public List<int>? DirectorIds { get; set; }
+    }
+}
