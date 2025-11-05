cbae2bb4ebb7a1362c2ec8b61a4f06a28893b17a
421301-1w1-llop-julian <421301@tecnicatura.frc.utn.edu.ar>
2025-11-04 23:22:01 -0300

    CRUD de peliculas

---
+++ backend/WebApplication1/DTOs/Pelicula/PeliculaDTO.cs
@@ -0,0 +1,25 @@
+﻿namespace WebApplication1.DTOs.Pelicula
+{
+    public class PeliculaDTO
+    {
+        public int IdPelicula { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Duracion { get; set; }
+        public DateOnly? FechaEstreno { get; set; }
+        public int IdClasificacion { get; set; }
+        public string Clasificacion { get; set; } // desde IdClasificacionNavigation
+        public int IdPais { get; set; }
+        public string Pais { get; set; } // desde IdPaisNavigation
+        public int IdDistribuidora { get; set; }
+        public string Distribuidora { get; set; } // desde IdDistribuidoraNavigation
+        public int IdTipoPublico { get; set; }
+        public string TipoPublico { get; set; } // desde IdTipoPublicoNavigation
+
+        // Relaciones opcionales (solo si las necesitás)
+        public List<string>? Generos { get; set; }
+        public List<string>? Idiomas { get; set; }
+        public List<string>? Actores { get; set; }
+        public List<string>? Directores { get; set; }
+    }
+}
---
+++ backend/WebApplication1/DTOs/Pelicula/PeliculaListDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.DTOs.Pelicula
+{
+    public class PeliculaListDTO
+    {
+        public int IdPelicula { get; set; }
+        public string Nombre { get; set; }
+        public string Clasificacion { get; set; }
+        public string Pais { get; set; }
+        public DateOnly? FechaEstreno { get; set; }
+    }
+}
